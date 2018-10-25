62bc8e28e38f36af2ab481826e7971b818eec7f9
EclipseUsername <2898847759@qq.com>
2018-10-25 15:43:54 +0800

    test

--- src/demo01/test1.java
+++ src/demo01/test1.java
@@ -1,7 +1,21 @@
 package demo01;
-
+/**
+ * 
+ * @author lzy
+ *
+ */
 public class test1 {
+	/**
+	 * 
+	 * @param args
+	 */
 public static void main(String[] args) {
+	/**
+	 * fdnoas'pe
+	 */
+	System.out.println("helloworld!");
+	System.out.println("helloworld!");
+	System.out.println("helloworld!");
 	System.out.println("helloworld!");
 }
 }
